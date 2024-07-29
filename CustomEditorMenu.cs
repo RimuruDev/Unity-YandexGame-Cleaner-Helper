b0e809c6f1bc6793ebcd589bee0c80baccd6fe90
Rimuru <85500556+RimuruDev@users.noreply.github.com>
2024-07-29 19:04:26 +0400

    Create CustomEditorMenu.cs

---
+++ CustomEditorMenu.cs
@@ -0,0 +1,40 @@
+// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: 
+//          - Gmail:    rimuru.dev@gmail.com
+//          - GitHub:   https://github.com/RimuruDev
+//          - LinkedIn: https://www.linkedin.com/in/rimuru/
+//
+// **************************************************************** //
+
+#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace AbyssMoth
+{
+    [HelpURL("https://github.com/RimuruDev/Unity-YandexGame-Cleaner-Helper")]
+    public sealed class CustomEditorMenu : MonoBehaviour
+    {
+        private const string folderPath = "Assets/YandexGame/WorkingData/Editor";
+
+        [MenuItem("_GameData/Delete YandexGame WorkingData Folder")]
+        private static void DeleteWorkingDataFolder()
+        {
+            if (Directory.Exists(folderPath))
+            {
+                Directory.Delete(folderPath, true);
+                File.Delete(folderPath + ".meta");
+                AssetDatabase.Refresh();
+                Debug.Log($"Deleted folder: {folderPath}");
+            }
+            else
+            {
+                Debug.LogWarning($"Folder not found: {folderPath}");
+            }
+        }
+    }
+}
+#endif
